471b940c06d8beb072ef784102f6e7497e841f63
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-05-08 21:37:23 +0300

    8 05 2022 second

--- js/click-show-image.js
+++ js/click-show-image.js
@@ -1,3 +1,4 @@
+//for screen
 var buttons = document.querySelectorAll(".sliderClickButton button");
 console.log("array cu buttons : ", buttons);
 
@@ -23,5 +24,34 @@ divPhoto.style.marginTop = "3em";
     divPhoto.firstElementChild.style.color = "#785807";
     divPhoto.firstElementChild.style.fontWeight = "bold";
       divPhoto.firstElementChild.style.textAlign = "center";
+  })
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//for mobile
+var buttonsMobile = document.querySelectorAll(".sliderClickButton.slider-mobile button");
+console.log("array cu buttons : ", buttons);
+
+var divPhotoMobile = document.querySelector("#containerImageMobile");
+console.log("div-ul cu clasa container in care vreau sa se afiseze cate o poza", divPhoto);
+
+divPhotoMobile.style.textAlign = "center";
+divPhotoMobile.style.backgroundPosition = "center";
+divPhotoMobile.style.backgroundSize = "cover";
+divPhotoMobile.style.backgroundRepeat = "no-repeat";
+
+for (var i = 0; i < buttonsMobile.length; i++) {
+
+  buttonsMobile[i].addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("string din data-src : ", this.dataset.src);
+
+  divPhotoMobile.style.backgroundImage = "url(" + this.dataset.src + ")";
+divPhotoMobile.style.height = "15em";
+divPhotoMobile.style.width = "20em";
+divPhotoMobile.style.marginTop = "3em";
+    divPhotoMobile.firstElementChild.innerText = this.dataset.text;
+    divPhotoMobile.firstElementChild.style.color = "#785807";
+    divPhotoMobile.firstElementChild.style.fontWeight = "bold";
+    divPhotoMobile.firstElementChild.style.textAlign = "center";
   })
 }
