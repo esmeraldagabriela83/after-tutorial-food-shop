e52f1a0e0ec9b8ac95f10cf9c3d7e5c0695ad045
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-05-16 16:54:53 +0300

    16 05 2022

--- js/click-show-image.js
+++ js/click-show-image.js
@@ -1,38 +1,39 @@
 //for screen
-var buttons = document.querySelectorAll(".sliderClickButton button");
-console.log("array cu buttons : ", buttons);
+var buttonsScreen = document.querySelectorAll(".sliderClickButton.slider-screen button");
+console.log("array cu buttons screen : ", buttonsScreen);
 
-var divPhoto = document.querySelector("#containerImage");
-console.log("div-ul cu clasa container in care vreau sa se afiseze cate o poza", divPhoto);
+var divPhotoScreen = document.querySelector("#containerImageScreen");
+console.log("div-ul cu clasa container in care vreau sa se afiseze cate o poza for mobile", divPhotoScreen);
 
-divPhoto.style.textAlign = "center";
-divPhoto.style.backgroundPosition = "center";
-divPhoto.style.backgroundSize = "cover";
-divPhoto.style.backgroundRepeat = "no-repeat";
+divPhotoScreen.style.textAlign = "center";
+divPhotoScreen.style.backgroundPosition = "center";
+divPhotoScreen.style.backgroundSize = "cover";
+divPhotoScreen.style.backgroundRepeat = "no-repeat";
 
-for (var i = 0; i < buttons.length; i++) {
+for (var i = 0; i < buttonsScreen.length; i++) {
 
-  buttons[i].addEventListener("click", function(event) {
+  buttonsScreen[i].addEventListener("click", function(event) {
     event.preventDefault();
     console.log("string din data-src : ", this.dataset.src);
 
-    divPhoto.style.backgroundImage = "url(" + this.dataset.src + ")";
-divPhoto.style.height = "22em";
-divPhoto.style.width = "30em";
-divPhoto.style.marginTop = "3em";
-    divPhoto.firstElementChild.innerText = this.dataset.text;
-    divPhoto.firstElementChild.style.color = "#785807";
-    divPhoto.firstElementChild.style.fontWeight = "bold";
-      divPhoto.firstElementChild.style.textAlign = "center";
+    divPhotoScreen.style.backgroundImage = "url(" + this.dataset.src + ")";
+    divPhotoScreen.style.height = "22em";
+    divPhotoScreen.style.width = "30em";
+    divPhotoScreen.style.marginTop = "3em";
+    divPhotoScreen.firstElementChild.innerText = this.dataset.text;
+    divPhotoScreen.firstElementChild.style.color = "#785807";
+    divPhotoScreen.firstElementChild.style.fontWeight = "bold";
+    divPhotoScreen.firstElementChild.style.textAlign = "center";
   })
 }
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //for mobile
 var buttonsMobile = document.querySelectorAll(".sliderClickButton.slider-mobile button");
-console.log("array cu buttons : ", buttons);
+console.log("array cu buttons mobile : ", buttonsMobile);
 
 var divPhotoMobile = document.querySelector("#containerImageMobile");
-console.log("div-ul cu clasa container in care vreau sa se afiseze cate o poza", divPhoto);
+console.log("div-ul cu clasa container in care vreau sa se afiseze cate o poza for mobile", divPhotoMobile);
 
 divPhotoMobile.style.textAlign = "center";
 divPhotoMobile.style.backgroundPosition = "center";
@@ -45,10 +46,10 @@ for (var i = 0; i < buttonsMobile.length; i++) {
     event.preventDefault();
     console.log("string din data-src : ", this.dataset.src);
 
-  divPhotoMobile.style.backgroundImage = "url(" + this.dataset.src + ")";
-divPhotoMobile.style.height = "15em";
-divPhotoMobile.style.width = "20em";
-divPhotoMobile.style.marginTop = "3em";
+    divPhotoMobile.style.backgroundImage = "url(" + this.dataset.src + ")";
+    divPhotoMobile.style.height = "15em";
+    divPhotoMobile.style.width = "20em";
+    divPhotoMobile.style.marginTop = "3em";
     divPhotoMobile.firstElementChild.innerText = this.dataset.text;
     divPhotoMobile.firstElementChild.style.color = "#785807";
     divPhotoMobile.firstElementChild.style.fontWeight = "bold";
--- js/image-slider.js
+++ js/image-slider.js
@@ -1,25 +1,36 @@
-/*image slider
- Javascript Image Slider with Next and Prev buttons | Html, Css and Javascript slider
- https://www.youtube.com/watch?v=uAAD3mmQGRQ&t=5s */
-
+////*image slider
+ ////https://www.youtube.com/watch?v=uAAD3mmQGRQ
+console.log("---------------------------------------------------image-slider.js");
 var slider_img=document.querySelector(".slider-img");
 
 var images=["slider-one-pizza.jpg" , "slider-two-pizza.jpg" , "slider-three-pizza.jpg" , "slider-fourth-pizza.jpg"];
-
+console.log("images array from image-slider.js is : " , images);
 var i=0 ;//current image index
 
-function prev(){
-if(i<=0) i=images.length;
-i--;
-return setImg();
-}
 
-function next(){
-  if(i>=images.length - 1) i=-1;
+var prevSLIDER=document.getElementById('prev-slider-btnPizza');
+console.log("prevSLIDER from image-slider.js is : " , prevSLIDER);
+prevSLIDER.addEventListener("click" , function(event){
+  event.preventDefault();
+  console.log("previous");
+  if(i<=0) i=images.length ;
+  i--;
+  return setImg();
+})
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+var nextSLIDER=document.getElementById('next-slider-btnPizza');
+console.log("nextSLIDER from image-slider.js is : " , nextSLIDER);
+nextSLIDER.addEventListener("click" , function(event){
+  event.preventDefault();
+  console.log("next");
+  if(i>=images.length -1) i=-1;
   i++;
   return setImg();
-}
+})
+///////////////////////////////////////////////////////////////////////////////////////////////////
 
 function setImg(){
-  return slider_img.setAttribute("src" , "images/" + images[i]);
+  return slider_img.setAttribute("src" , "./images/" + images[i]);
 }
